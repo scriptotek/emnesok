scriptotek/emnesok

commit 981409bafee7258bbeb3e0af9b94f84c0d129693

Store library selection in URL

app/home/catalogue.js
@@ -94,11 +94,17 @@
         vm.expandGroup = expandGroup;
         vm.getMoreRecords = getMoreRecords;
 
-        vm.institutions = Config.institutions;
-        vm.selectedInstitution = Session.selectedInstitution;
-        vm.selectedLibrary = Session.selectedLibrary;
         vm.selectInstitution = selectInstitution;
         vm.selectLibrary = selectLibrary;
+
+        vm.institutions = Config.institutions;
+        if ($stateParams.library && $stateParams.library.indexOf(':') != -1) {
+            vm.selectedInstitution = $stateParams.library.split(':')[0];
+            vm.selectedLibrary = $stateParams.library.split(':')[1];
+        } else {
+            vm.selectedInstitution = $stateParams.library;
+            vm.selectedLibrary = null;
+        }
 
         vm.controlledSearch = ($stateParams.narrow == 'true');
         vm.updateControlledSearch = updateControlledSearch;
@@ -167,8 +173,8 @@
         }
 
         function search() {
-            var inst = vm.selectedInstitution ? vm.selectedInstitution.id : null;
-            var lib = vm.selectedLibrary ? vm.selectedLibrary.id : null;
+            var inst = vm.selectedInstitution ? vm.selectedInstitution : null;
+            var lib = vm.selectedLibrary ? vm.selectedInstitution + vm.selectedLibrary : null;
             var vocab = subject.data.type == 'Geographic' ? 'geo' : vm.controlledSearch ? vm.vocab : '';
             vm.busy = true;
             Catalogue.search(vocab, vm.term, vm.next, inst, lib).then(
@@ -187,16 +193,11 @@
         }
 
         function selectInstitution(institution) {
-            Session.selectInstitution(institution);
-            vm.selectedInstitution = institution;
-            vm.selectedLibrary = null;
-            searchFromStart();
+            $state.go('subject.search', {library: institution});
         }
 
         function selectLibrary(library) {
-            Session.selectLibrary(library);
-            vm.selectedLibrary = library;
-            searchFromStart();
+            $state.go('subject.search', {library: vm.selectedInstitution + ':' + library});
         }
 
         function updateControlledSearch() {

app/app.module.js
@@ -87,7 +87,7 @@
 			}
 		})
 		.state('subject.search', {
-			url: '/search?term&id&narrow',
+			url: '/search?term&id&narrow&library',
 			views: {
 				'catalogue': {
 					templateUrl: './templates/catalogue.html',

app/home/config.service.js
@@ -1,5 +1,5 @@
 (function() {
-    'use strict';
+	'use strict';
 
 	angular
 		.module('app.services.config', [])
@@ -16,18 +16,35 @@
 				la: 'Latin'
 			},
 			defaultLanguage: 'nb',
-			institutions: [
-	            {id: 'UBO', label: 'UiO', libraries: [
-		            {id: 'ubo1030310', label: 'Realfagsbiblioteket VB'},
-		            {id: 'ubo1030317', label: 'Informatikkbiblioteket'},
-		            {id: 'ubo1030500', label: 'NHM'},
-		            {id: 'ubo1030300,1030303', label: 'Humsam-biblioteket + Sophus Bugge'}
-		        ]},
-	            {id: 'UBB', label: 'UiB'},
-	            {id: 'NTNU_UB', label: 'NTNU'},
-	            {id: 'UBTO', label: 'UiT'},
-	            {id: 'NMBU', label: 'NMBU'}
-	        ],
+			institutions: {
+				'UBO': {
+					label: 'UiO',
+					libraries: {
+						'1030310': {label: 'Realfagsbiblioteket VB'},
+						'1030317': {label: 'Informatikkbiblioteket'},
+						'1030500': {label: 'NHM'},
+						'1030300,1030303': {label: 'Humsam-biblioteket + Sophus Bugge'}
+					}
+				},
+				'UBB': {
+					label: 'UiB',
+					libraries: {
+						'1120104': {label: 'Bibliotek for realfag'},
+						'1120100': {label: 'Bibliotek for humaniora'},
+						'1120108': {label: 'Bibliotek for amfunnsvitenskap'}
+					}
+				},
+				'NTNU_UB': {
+					label: 'NTNU',
+					libraries: {
+						'1160133': {label: 'DORA'},
+						'1160103': {label: 'Realfagbiblioteket'},
+						'1160101': {label: 'Teknologibiblioteket'}
+					}
+				},
+				'UBTO': {label: 'UiT'},
+				'NMBU': {label: 'NMBU'}
+			},
 			vocabularies: {
 				realfagstermer: {
 					name: 'Realfagstermer',

templates/catalogue.html
@@ -16,23 +16,23 @@ Next: {{ vm.next }}
 
 	    <div class="btn-group" uib-dropdown uib-keyboard-nav>
 	        <button id="simple-btn-keyboard-nav" type="button" class="btn btn-default" uib-dropdown-toggle>
-	            {{ vm.selectedInstitution ? vm.selectedInstitution.label : 'All libraries' | translate }} <span class="caret"></span>
+	            {{ vm.selectedInstitution ? vm.institutions[vm.selectedInstitution].label : 'All libraries' | translate }} <span class="caret"></span>
 	        </button>
 	        <ul class="uib-dropdown-menu dropdown-menu-right" role="menu" aria-labelledby="simple-btn-keyboard-nav">
 	            <li role="menuitem"><a href ng-click="vm.selectInstitution(null)" translate>All libraries</a></li>
 	            <li class="divider"></li>
-	            <li role="menuitem" ng-repeat="institution in vm.institutions"><a href ng-click="vm.selectInstitution(institution)">{{ institution.label }}</a></li>
+	            <li role="menuitem" ng-repeat="(key, val) in vm.institutions"><a href ng-click="vm.selectInstitution(key)">{{ val.label }}</a></li>
 	        </ul>
 	    </div>
 
-	    <div class="btn-group" ng-show="vm.selectedInstitution && vm.selectedInstitution.libraries" uib-dropdown uib-keyboard-nav uib-tooltip="Merk: Hvis du avgrenser på avdeling vil du ikke få treff i e-bøker.">
+	    <div class="btn-group" ng-show="vm.selectedInstitution && vm.institutions[vm.selectedInstitution].libraries" uib-dropdown uib-keyboard-nav uib-tooltip="Merk: Hvis du avgrenser på avdeling vil du ikke få treff i e-bøker.">
 	        <button id="simple-btn-keyboard-nav2" type="button" class="btn btn-default" uib-dropdown-toggle>
-	            {{ vm.selectedLibrary ? vm.selectedLibrary.label : 'Alle avdelinger' }} <span class="caret"></span>
+	            {{ vm.selectedLibrary ? vm.institutions[vm.selectedInstitution].libraries[vm.selectedLibrary].label : 'Alle avdelinger' }} <span class="caret"></span>
 	        </button>
 	        <ul class="uib-dropdown-menu dropdown-menu-right" role="menu" aria-labelledby="simple-btn-keyboard-nav2">
 	            <li role="menuitem"><a href ng-click="vm.selectLibrary(null)">Alle avdelinger</a></li>
 	            <li class="divider"></li>
-	            <li role="menuitem" ng-repeat="library in vm.selectedInstitution.libraries"><a href ng-click="vm.selectLibrary(library)">{{ library.label }}</a></li>
+	            <li role="menuitem" ng-repeat="(key, val) in vm.institutions[vm.selectedInstitution].libraries"><a href ng-click="vm.selectLibrary(key)">{{ val.label }}</a></li>
 	        </ul>
 	    </div>
 	</div>

commit 17ce54ae4a5a346893b5e48bd2ca66808d46f645

Fix library URL if library is null
Bug introduced in 981409ba

app/home/catalogue.js
@@ -224,7 +224,11 @@
         }
 
         function selectLibrary(library) {
-            $state.go('subject.search', {library: vm.selectedInstitution + ':' + library});
+            if (library) {
+                $state.go('subject.search', {library: vm.selectedInstitution + ':' + library});
+            } else {
+                $state.go('subject.search', {library: vm.selectedInstitution});
+            }
         }
 
         function updateControlledSearch() {