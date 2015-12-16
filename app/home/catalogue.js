scriptotek/emnesok

commit f30bf3b13863fc7e238451a8bf9b582e53b02742

Consistency: use broad/narrow everywhere
And use 'broad' rather than 'narrow' as the url parameter to reflect that we can expand the search from the standard.

app/home/catalogue.js
@@ -129,7 +129,11 @@
             vm.selectedLibrary = null;
         }
 
-        vm.controlledSearch = ($stateParams.narrow == 'true');
+        var bs = gettext('broad search');
+        var ns = gettext('narrow search');
+        vm.broadSearch = ($stateParams.broad === undefined) ? true : ($stateParams.broad == 'true');
+        vm.searchType = vm.broadSearch ? gettextCatalog.getString(bs) : gettextCatalog.getString(ns);
+
         vm.updateControlledSearch = updateControlledSearch;
 
         activate();
@@ -222,7 +226,7 @@
         function search() {
             var inst = vm.selectedInstitution ? vm.selectedInstitution : null;
             var lib = vm.selectedLibrary ? vm.selectedInstitution + vm.selectedLibrary : null;
-            var vocab = subject.data.type == 'Geographic' ? 'geo' : vm.controlledSearch ? vm.vocab : '';
+            var vocab = subject.data.type == 'Geographic' ? 'geo' : vm.broadSearch ? vm.vocab : '';
             var defaultLang = Lang.defaultLanguage;
             var query = subject.data.prefLabel[defaultLang];
             if (subject.data.prefLabel.en !== undefined && subject.data.prefLabel.en !== subject.data.prefLabel[defaultLang]) {
@@ -260,7 +264,7 @@
         }
 
         function updateControlledSearch() {
-            $state.go('subject.search', {narrow: vm.controlledSearch});
+            $state.go('subject.search', {broad: vm.broadSearch});
         }
 
     }

app/app.module.js
@@ -104,7 +104,7 @@
 			}
 		})
 		.state('subject.search', {
-			url: '/search?term&id&narrow&library',
+			url: '/search?term&id&broad&library',
 			views: {
 				'catalogue': {
 					templateUrl: './templates/catalogue.html',

templates/catalogue.html
@@ -10,8 +10,8 @@ Next: {{ vm.next }}
 	 <div style="float:right;">
 
 		<div class="btn-group" ng-show="vm.vocab && vm.subject.data.type != 'Geographic'">
-	        <label class="btn btn-default" ng-model="vm.controlledSearch" ng-change="vm.updateControlledSearch()" uib-btn-radio="false" uib-tooltip="Search all subject vocabularies" translate>Broad</label>
-	        <label class="btn btn-default" ng-model="vm.controlledSearch" ng-change="vm.updateControlledSearch()" uib-btn-radio="true" uib-tooltip="Search {{vm.vocab}} only." translate>Narrow</label>
+	        <label class="btn btn-default" ng-model="vm.broadSearch" ng-change="vm.updateControlledSearch()" uib-btn-radio="true" uib-tooltip="Search all subject vocabularies" translate>Broad</label>
+	        <label class="btn btn-default" ng-model="vm.broadSearch" ng-change="vm.updateControlledSearch()" uib-btn-radio="false" uib-tooltip="Search {{vm.vocab}} only." translate>Narrow</label>
 	    </div>
 
 	    <div class="btn-group" uib-dropdown uib-keyboard-nav>
@@ -49,13 +49,13 @@ Next: {{ vm.next }}
 		<span translate>
 			How odd! Not a single document on that subject could be found using the current subject vocabulary.
 		</span>
-		<span ng-show="vm.controlledSearch && vm.subject.data.type !== 'Geographic'" translate>
+		<span ng-show="!vm.broadSearch && vm.subject.data.type !== 'Geographic'" translate>
 			You could try expanding the search to a <a ui-sref="subject.search({narrow:false})">broad search</a>.
 		</span>
 	</p>
 
 	<p ng-show="vm.start && vm.total_results"
-	   translate translate-n="vm.total_results" translate-plural="Found {{$count}} documents.">Found one document.
+	   translate translate-n="vm.total_results" translate-plural="Found {{vm.total_results}} documents about this topic using {{vm.searchType}}.">Found one document about this topic using {{vm.searchType}}.
 	</p>
 
 	<ul class="list-group">

commit e1885f5614267c83592e47777e4ffa6bff985ee7

Fix broad/narrow search
Bug introduced in f30bf3b1

app/home/catalogue.js
@@ -226,7 +226,7 @@
         function search() {
             var inst = vm.selectedInstitution ? vm.selectedInstitution : null;
             var lib = vm.selectedLibrary ? vm.selectedInstitution + vm.selectedLibrary : null;
-            var vocab = subject.data.type == 'Geographic' ? 'geo' : vm.broadSearch ? vm.vocab : '';
+            var vocab = subject.data.type == 'Geographic' ? 'geo' : vm.broadSearch ? '' : vm.vocab;
             var defaultLang = Lang.defaultLanguage;
             var query = subject.data.prefLabel[defaultLang];
             if (subject.data.prefLabel.en !== undefined && subject.data.prefLabel.en !== subject.data.prefLabel[defaultLang]) {