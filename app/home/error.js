49856556be0d7f01e5b1db4f9516e8ef7e2e74c0
Dan Michael O. Heggø <danmichaelo@gmail.com>
2015-12-16 19:06:15 +0100

    Update translatable strings and translations

--- app/home/error.js
+++ app/home/error.js
@@ -3,17 +3,21 @@
 
 	angular
 		.module('app.modules.error', [])
-		.controller('ErrorController', ['$state', '$stateParams', controller])
+		.controller('ErrorController', controller)
 		;
 
-	function controller($state, $stateParams) {
+	controller.$inject = ['$state', '$stateParams', 'gettext', 'gettextCatalog'];
+
+	function controller($state, $stateParams, gettext, gettextCatalog) {
 		/*jshint validthis: true */
 		var vm = this;
 
 		if ($state.get('error').error) {
 			vm.message = $state.get('error').error.message;
 		} else {
-			vm.message = 'An unknown error occured';
+            var msg = gettext('An unknown error occured.');
+            var translated = gettextCatalog.getString(msg);
+			vm.message = translated;
 		}
 
 	}
