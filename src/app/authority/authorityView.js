scriptotek/emnesok

commit efdd3d61c4b87d95084ad1011cc39bfb4ee99330

Sort lists of narrower/broader/related concepts

src/app/authority/authorityView.js
@@ -29,9 +29,11 @@ function AuthorityViewController($scope, $state, $stateParams, $timeout, $rootSc
     function activate() {
         vm.history = AuthorityService.history;
         vm.currentSubject = AuthorityService.currentSubject;
+        AuthorityService.currentSubject.sortLabels();
         AuthorityService.onSubject($scope, function () {
             vm.history = AuthorityService.history;
             vm.currentSubject = AuthorityService.currentSubject;
+            AuthorityService.currentSubject.sortLabels();
         });
     }
 }

src/app/authority/authorityRecord.html
@@ -38,21 +38,21 @@
                 <tr ng-show="vm.subject.get('related').length">
                     <th translate>See also:</th>
                     <td>
-                        <a ng-repeat="related in vm.subject.get('related') | orderBy:'prefLabel'" class="subject-tag" ng-class="{ geographic: related.type == 'Geographic', genre: related.type == 'GenreForm' }" ui-sref="subject.search({ term: null, id: null, uri: related.uri })">{{ related.notation }} {{ related.getPrefLabel() }}</a>
+                        <a ng-repeat="related in vm.subject.get('related') | orderBy:'prefLabel'" class="subject-tag" ng-class="{ geographic: related.type == 'Geographic', genre: related.type == 'GenreForm' }" ui-sref="subject.search({ term: null, id: null, uri: related.uri })">{{ related.getPrefLabel() }}</a>
                     </td>
                 </tr>
 
                 <tr class="block" ng-show="vm.subject.broader.length">
                     <th translate>Broader:</th>
                     <td>
-                        <a ng-repeat="broader in vm.subject.broader | orderBy:'prefLabel'" class="subject-tag" ng-class="{ geographic: broader.type == 'Geographic', genre: broader.type == 'GenreForm' }" ui-sref="subject.search({ term: null, id: null, uri: broader.uri })">{{broader.notation }} {{ broader.getPrefLabel() }}</a>
+                        <a ng-repeat="broader in vm.subject.broader | orderBy:'prefLabel'" class="subject-tag" ng-class="{ geographic: broader.type == 'Geographic', genre: broader.type == 'GenreForm' }" ui-sref="subject.search({ term: null, id: null, uri: broader.uri })">{{ broader.getPrefLabel() }}</a>
                     </td>
                 </tr>
 
                 <tr class="block" ng-show="vm.subject.narrower.length">
                     <th translate>Narrower:</th>
                     <td>
-                        <a ng-repeat="narrower in vm.subject.narrower | orderBy:'prefLabel'" class="subject-tag" ng-class="{ geographic: narrower.type == 'Geographic', genre: narrower.type == 'GenreForm' }" ui-sref="subject.search({ term: null, id: null, uri: narrower.uri })">{{ narrower.notation }} {{ narrower.getPrefLabel() }}</a>
+                        <a ng-repeat="narrower in vm.subject.narrower | orderBy:'prefLabel'" class="subject-tag" ng-class="{ geographic: narrower.type == 'Geographic', genre: narrower.type == 'GenreForm' }" ui-sref="subject.search({ term: null, id: null, uri: narrower.uri })">{{ narrower.getPrefLabel() }}</a>
                     </td>
                 </tr>
 

src/app/services/authority.service.js
@@ -1,6 +1,8 @@
 import angular from 'angular';
 import configModule from './config.service';
+import {cloneDeep} from 'lodash/lang';
 import {get} from 'lodash/object';
+import {sortBy} from 'lodash/collection';
 import * as log from 'loglevel';
 
 const moduleName = 'app.services.authority';
@@ -46,7 +48,13 @@ class Subject {
 
         ['_components', 'broader', 'narrower', 'related'].forEach((prop) => {
             if (data[prop] !== undefined) {
-                this[prop] = (get(data, prop) || []).map((res) => new Subject(Config, langService, res));
+                this[prop] = (get(data, prop) || []).map((res) => {
+
+                    // Assume concepts follow the same scheme (would be better if the server response told us explicitly though)
+                    res.inScheme = cloneDeep(data.inScheme);
+
+                    return new Subject(Config, langService, res);
+                });
             }
         });
 
@@ -106,6 +114,15 @@ class Subject {
         this.vocab = this.vocabulary.code; // for backwards compat
 
         this.data = data; // Keep a copy
+    }
+
+    sortLabels() {
+        // Sorting will depend on selected language, so we cannot just sort in the constructor.
+        ['_components', 'broader', 'narrower', 'related'].forEach((prop) => {
+            if (this[prop] !== undefined) {
+                this[prop] = sortBy(this[prop], [x => x.getPrefLabel()]);
+            }
+        });
     }
 
     get(key, def = null) {

commit 55b314e6261268d0e875889818f6e55679b2b3f1

Fix bug introduced in efdd3d6

src/app/authority/authorityView.js
@@ -26,15 +26,17 @@ function AuthorityViewController($scope, $state, $stateParams, $timeout, $rootSc
 
     /////
 
-    function activate() {
+    function updateSubject() {
         vm.history = AuthorityService.history;
         vm.currentSubject = AuthorityService.currentSubject;
-        AuthorityService.currentSubject.sortLabels();
-        AuthorityService.onSubject($scope, function () {
-            vm.history = AuthorityService.history;
-            vm.currentSubject = AuthorityService.currentSubject;
-            AuthorityService.currentSubject.sortLabels();
-        });
+        if (vm.currentSubject) {
+            vm.currentSubject.sortLabels();
+        }
+    }
+
+    function activate() {
+        updateSubject();
+        AuthorityService.onSubject($scope, updateSubject);
     }
 }
 